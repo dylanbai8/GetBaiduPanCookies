78aa22ef21235406e12084b2f3f4ccfd2f2ec511
yude268 <yude268@qq.com>
2021-08-08 15:51:10 +0800

    Signed-off-by: yude268 <yude268@qq.com>

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ document.addEventListener('DOMContentLoaded', function() {
         name: 'BDUSS'
       },
       function(cookies) {
+        var BAIDU_BDUSS = "";
+        for (var i in cookies) {
         var BAIDU_BDUSS = cookies[0].value;
+        }
+        if (!BAIDU_BDUSS) {BAIDU_BDUSS = "您未登录百度网盘";}
         document.getElementById("cookies-BDUSS").innerText = BAIDU_BDUSS;
       }
     );
@@ -18,7 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
         name: 'STOKEN'
       },
       function(cookies) {
+        var BAIDU_STOKEN = "";
+        for (var i in cookies) {
         var BAIDU_STOKEN = cookies[0].value;
+        }
+        if (!BAIDU_STOKEN) {BAIDU_STOKEN = "您未登录百度网盘";}
         document.getElementById("cookies-STOKEN").innerText = BAIDU_STOKEN;
       }
     );
@@ -32,6 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
         for (var i in cookies) {
           BAIDU_ALL += cookies[i].name + "=" + cookies[i].value + ";";
         }
+        if (!BAIDU_ALL) {BAIDU_ALL = "您未登录百度网盘";}
         document.getElementById("cookies-ALL").innerText = BAIDU_ALL;
       }
     );
